a1473ee0a295022614d05b5479fd7b196a49fb22
adz5a <antoine.d.zimmermann@gmail.com>
2018-11-19 15:43:38 +0100

    Add doc

--- tools.sql
+++ tools.sql
@@ -1,7 +1,31 @@
+\d pg_catalog.pg_stat_activity
+/*
+                      View "pg_catalog.pg_stat_activity"
+      Column      |           Type           | Collation | Nullable | Default 
+------------------+--------------------------+-----------+----------+---------
+ datid            | oid                      |           |          | 
+ datname          | name                     |           |          | 
+ pid              | integer                  |           |          | 
+ usesysid         | oid                      |           |          | 
+ usename          | name                     |           |          | 
+ application_name | text                     |           |          | 
+ client_addr      | inet                     |           |          | 
+ client_hostname  | text                     |           |          | 
+ client_port      | integer                  |           |          | 
+ backend_start    | timestamp with time zone |           |          | 
+ xact_start       | timestamp with time zone |           |          | 
+ query_start      | timestamp with time zone |           |          | 
+ state_change     | timestamp with time zone |           |          | 
+ waiting          | boolean                  |           |          | 
+ state            | text                     |           |          | 
+ query            | text                     |           |          | 
+*/
+
 
 -- Sélectionne les connexions qui ne sont pas 'idle' et montre les query quelles
 -- sont en train d'exécuter
 select
+    pid,
     state,
     query,
     datname,
@@ -11,12 +35,43 @@ select
 from pg_catalog.pg_stat_activity
 where state <> 'idle'
 
+
+-- Kill a connexion
+SELECT
+    pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE (
+    datname = 'databasename'
+    AND pid <> pg_backend_pid()
+    AND state in ('idle')
+);
+
 -- Select query and join on DB to see locks
 
 
 \d pg_catalog.pg_locks
-\d pg_catalog.pg_class
+/*
+                   View "pg_catalog.pg_locks"
+       Column       |   Type   | Collation | Nullable | Default 
+--------------------+----------+-----------+----------+---------
+ locktype           | text     |           |          | 
+ database           | oid      |           |          | 
+ relation           | oid      |           |          | 
+ page               | integer  |           |          | 
+ tuple              | smallint |           |          | 
+ virtualxid         | text     |           |          | 
+ transactionid      | xid      |           |          | 
+ classid            | oid      |           |          | 
+ objid              | oid      |           |          | 
+ objsubid           | smallint |           |          | 
+ virtualtransaction | text     |           |          | 
+ pid                | integer  |           |          | 
+ mode               | text     |           |          | 
+ granted            | boolean  |           |          | 
+ fastpath           | boolean  |           |          | 
+*/
 select
+    pg_sa.query,
     pg_l.database,
     pg_l.mode,
     pg_c.relname,
